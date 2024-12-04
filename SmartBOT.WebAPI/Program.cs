9ca845b77d3bdedf50fbb222e2e925643e0d2f18
Marcelo Gulias <guliasmarcelo@gmail.com>
2024-12-04 12:31:48 -0300

    Add Swegger

--- SmartBOT.WebAPI/Program.cs
+++ SmartBOT.WebAPI/Program.cs
@@ -12,9 +12,15 @@ builder.Services.AddSingleton<IVectorDbSearchService, AzureAISearchService>();
 builder.Services.AddSingleton<IChatHistoryRepository, SqLiteChatHistoryRepository>();
 builder.Services.AddSingleton<IHelpDeskService, TeslaHelpDeskService>();
 
+// swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(); // Interface gráfica do Swagger
+app.UseRouting();
 
 // Configurar middlewares
 app.UseRouting();
